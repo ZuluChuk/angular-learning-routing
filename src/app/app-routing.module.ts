797a67ec8fca3616e63e8ff5219526bbd7666281
Stephen Rado <stephen.rado@ecx.io>
2019-06-12 11:33:20 +0100

    routes edit server and auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import {HomeComponent} from './home/home.component';
 import {UsersComponent} from './users/users.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {UserComponent} from './users/user/user.component';
+import {AuthGuard} from './auth-guard.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,8 +16,8 @@ const appRoutes: Routes = [
       { path: ':id/:name', component: UserComponent },
     ]
   },
-  { path: 'servers', component: ServersComponent, children: [
-      { path: ':id/edit', component: EditServerComponent },
+  { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
       { path: ':id', component: ServerComponent }
     ]
   },
