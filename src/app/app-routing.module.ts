a3678576cc029ebe1c269ec4d8d103f8ca15beb1
Stephen Rado <stephen.rado@ecx.io>
2019-06-12 14:45:55 +0100

    fixing issue with resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import {UserComponent} from './users/user/user.component';
 import {AuthGuard} from './auth-guard.service';
 import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
 import {ErrorPageComponent} from './error-page/error-page.component';
+import {ServerResolver} from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,8 +19,8 @@ const appRoutes: Routes = [
     ]
   },
   { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
-      { path: ':id', component: ServerComponent }
     ]
   },
   // { path: 'not-found', component: PageNotFoundComponent},
